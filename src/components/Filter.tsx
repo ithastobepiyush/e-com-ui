e28b4971dc3e3d15f07b36e461b960c61155cf25
Piyush Tiwari <ithastobepiyush@gmail.com>
2026-02-25 19:02:05 +0530

    Filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -6,14 +6,27 @@ const Filter = () => {
     const searchParams = useSearchParams()
     const router = useRouter()
     const pathname = usePathname()
+    
+    const handleFilter = (value: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.set("sort", value);
+      router.push(`${pathname}?${params.toString()}`, { scroll:false});
+    };
+
   return (
     <div className='flex items-center justify-end gap-2 text-sm text-gray-500 my-6'>
         <span>Sort by</span>
-        <select name="sort" id="sort" className='ring-1 rounded-sm ring-gray-200 shadow-md p-1'>
+        <select
+        name="sort"
+        id="sort"
+        className='ring-1 rounded-sm ring-gray-200 shadow-md p-1'
+        onChange={(e) => handleFilter(e.target.value)}
+        >
             <option value="newest">Newest</option>
             <option value="oldest">Oldest</option>
             <option value="asc">Price: Low to High</option>
             <option value="dec">Price: High to Low</option>
+            
         </select>
     </div>
   )
